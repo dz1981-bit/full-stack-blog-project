3b31e11a8d14348e8cf9cc30766049c2f0639072
dz1981-bit <116315113+dz1981-bit@users.noreply.github.com>
2025-09-23 20:32:11 +0000

    chore: added api routes, body-parser, and cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,16 @@
 import express from 'express'
-// import { postsRoutes } from './routes/posts.js'
-// import bodyParser from 'body-parser'
-// import cors from 'cors'
+import { postsRoutes } from './routes/posts.js'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app = express()
-// app.use(bodyParser.json())
-// app.use(cors())
+app.use(bodyParser.json())
+app.use(cors())
 
-// postsRoutes(app)
+postsRoutes(app)
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express!')
+  res.send('Hello from Express!!!!')
 })
 
 export { app }
