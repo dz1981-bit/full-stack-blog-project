2daa015e8673e9f23e7e92917c0fae571c980d80
dz1981-bit <116315113+dz1981-bit@users.noreply.github.com>
2025-10-14 16:42:53 +0000

    chore: added dockerfiles

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,8 @@ import dotenv from 'dotenv'
 import { initDatabase } from './db/init.js'
 dotenv.config()
 
-// try {
 await initDatabase()
-// const PORT = process.env.PORT
+
 const PORT = process.env.PORT
 app.listen(PORT)
 console.info(`express server running on http://localhost:${PORT}`)
-// } catch (err) {
-//     console.error('error connecting to database:', err)
-// }
